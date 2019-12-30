e7ed062882cca3be06ce43e96dfb1e263ff83180
Suwa, Tamon <suwatamon@gmail.com>
2019-12-30 10:45:56 +0900

    Added number recognition
    Co-authored-by: StealthMajor2020 <kimaru.guy@gmail.com>

---
+++ tensor/app.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+model = load_model('cnn.h5')
+graph = tf.get_default_graph()
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    req = [int(i) for i in request.form['image'].split(',')]
+    img = np.array(req, dtype=np.float32).reshape(1,28,28,1)
+    with graph.as_default():
+        result = np.argmax(model.predict(img))
+    return str(result)
+
+@app.route('/')
+def hello_world():
+    with open('index.html', "rb") as f:
+        s = f.read()
+        return s
+
+if __name__ == '__main__':
+    app.run(debug=True)
