6532f7c67c2cf369889b01fa7179e9d4cb19f480
Suwa, Tamon <suwatamon@gmail.com>
2021-02-07 03:48:30 +0000

    Added html_test.go

--- html.go
+++ html.go
@@ -8,16 +8,17 @@ import (
 )
 
 func generateHTML(w http.ResponseWriter, isbnArg string) {
+	if len(isbnArg) == 13 {
+		isbnArg = isbn.Isbn13to10(isbnArg)
+	}
+
 	type tmplData struct {
 		Isbn string
 	}
 
 	t := template.Must(template.ParseFiles("reply.html"))
 
-	if len(isbnArg) == 13 {
-		isbnArg = isbn.Isbn13to10(isbnArg)
-	}
-
+	// setTemplateData
 	d := tmplData{Isbn: isbnArg}
 
 	// テンプレートを描画
@@ -25,3 +26,5 @@ func generateHTML(w http.ResponseWriter, isbnArg string) {
 		log.Fatal(err)
 	}
 }
+
+func setTemplateData() {}
---
+++ html_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+// TestSubstitutionOfISBNStringToTmplateData は
+// テンプレートにISBN文字列を代入するテスト
+func TestSubstitutionOfISBNStringToTmplateData(t *testing.T) {
+	setTemplateData()
+}
