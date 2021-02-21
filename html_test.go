f3a6cbe93e2e4a207642a86cc2200699d933960a
Suwa, Tamon <suwatamon@gmail.com>
2021-02-21 03:01:17 +0000

    revised test pattern

--- html_test.go
+++ html_test.go
@@ -20,16 +20,23 @@ func TestSubstitutionOfISBN10StringToTemplateData(t *testing.T) {
 }
 
 func TestSubstitutionOfISBN13StringToTemplateData(t *testing.T) {
-	// isbnArg の定義
-	// ISBN-10 : 4065216982
-	// ISBN-13 : 978-4065216989
-	isbnArg := "9784065216989"
-	d := setTemplateData(isbnArg)
-	// d のチェック
-	if d.Isbn10 != "4065216982" {
-		t.Errorf("Returnd ISBN10 of %s is not %s: %s\n", isbnArg, "4065216982", d.Isbn10)
+	cases := []struct {
+		input    string
+		expected tmplData
+	}{
+		{"9784065216989", tmplData{Isbn10: "4065216982", Isbn13: "9784065216989"}},
+		{"9784621300251", tmplData{Isbn10: "4621300253", Isbn13: "9784621300251"}},
 	}
-	if d.Isbn13 != "9784065216989" {
-		t.Errorf("Returnd ISBN13 of %s is not %s: %s\n", isbnArg, "9784065216989", d.Isbn13)
+
+	for _, tc := range cases {
+		// execute
+		d := setTemplateData(tc.input)
+		if d.Isbn10 != tc.expected.Isbn10 {
+			t.Errorf("Returnd ISBN10 of %s is not %s: %s\n", tc.input, tc.expected.Isbn10, d.Isbn10)
+		}
+		if d.Isbn13 != tc.expected.Isbn13 {
+			t.Errorf("Returnd ISBN13 of %s is not %s: %s\n", tc.input, tc.expected.Isbn13, d.Isbn13)
+		}
 	}
+
 }
